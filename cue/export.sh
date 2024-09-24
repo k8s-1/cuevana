e5f12bc8c80fb9911b66b5d6cea998e6d141b461
pman <pman-$-432894328923@gmail.com>
2024-09-24 16:37:24 +0200

    chore: update file(s): export.sh

--- cue/export.sh
+++ cue/export.sh
@@ -7,11 +7,11 @@ cd configs || exit 1
 for dir in *
 do
   
-  mkdir -p "$target"
+  mkdir -p "$target/$dir"
 
   for file in "$dir"/*.cue
   do
-    cue export -t dev "$file" --out yaml > "$target/${file%.cue}.yaml"
+    cue export -t dev "$file" --out yaml > "$target/$dir/${file%.cue}.yaml"
   done
 
 done
